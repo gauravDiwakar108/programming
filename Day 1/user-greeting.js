b5352eaefee660b448bed2c0d43eaa9bbedfeb86
Gaurav <gauravsubhash9999@gmail.com>
2026-08-28 20:30:13 +0530

    completed day 'puzzle 1'

---
+++ Day 1/user-greeting.js
@@ -0,0 +1,9 @@
+const greetUser = (name) => {
+    return `Hello, ${name}! Welcome to JavaScript`;
+}
+
+console.log(greetUser('Gaurav'));
+console.log(greetUser('Saurav'));
+console.log(greetUser('Virat'));
+
+// const greetUser = (name) => `Hello, ${name}! Welcome to JavaScript`;
