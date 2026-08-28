79ea084bec1dceb02c45cb4f244f4bed36104527
Gaurav <gauravsubhash9999@gmail.com>
2026-08-28 20:53:15 +0530

    completed 4 'count-even-numbers'

---
+++ Day 1/3-count-even-numbers.js
@@ -0,0 +1,11 @@
+const countEvenNumbers = (numbers) => {
+    let count = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 === 0) {
+            count += 1;
+        }
+    }
+    return count;
+};
+
+console.log(countEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
