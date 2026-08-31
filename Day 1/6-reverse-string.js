158fc4d605d64f448f3e0d4bc343feae9eee2c1e
Gaurav <gauravsubhash9999@gmail.com>
2026-09-01 01:43:06 +0530

    completed 'reverse string'

---
+++ Day 1/6-reverse-string.js
@@ -0,0 +1,10 @@
+const reverseString = (text) => {
+    let reverse = "";
+
+    for (let i=text.length-1; i>=0; i--){
+        reverse += text[i];
+    }
+    return reverse;
+}
+
+console.log(reverseString("Gaurav"));
