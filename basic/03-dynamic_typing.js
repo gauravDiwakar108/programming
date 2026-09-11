250418039560b1874d9b924281825cb636194b11
Gaurav <gauravsubhash9999@gmail.com>
2026-09-11 16:08:47 +0530

    done

---
+++ basic/03-dynamic_typing.js
@@ -0,0 +1,22 @@
+// dynamic typing by assigning a different data type to the same variable one after another
+
+let value = 10;
+console.log(value, typeof value);
+
+value = "Hello";
+console.log(value, typeof value);
+
+value = true;
+console.log(value, typeof value);
+
+value = null;
+console.log(value, typeof value);
+
+value = [1, 2, 3];
+console.log(value, typeof value);
+
+value = {
+    name: "Rahul",
+    age: 20
+};
+console.log(value, typeof value);
