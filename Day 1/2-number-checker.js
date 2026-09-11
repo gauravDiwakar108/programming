6abecf4848111843abe9de9a7f1431ba1adba27c
Gaurav <gauravsubhash9999@gmail.com>
2026-09-11 15:44:05 +0530

    starting new

--- Day 1/2-number-checker.js
+++
@@ -1,9 +0,0 @@
-const checkNumber = (number) => {
-    if (number>0) return `${number} is positive`;
-    else if (number<0) return `${number} is negative`;
-    else return `${number} is Zero`;
-}
-
-console.log(checkNumber(0));
-console.log(checkNumber(1));
-console.log(checkNumber(-1));
