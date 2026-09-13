c2e91f50b6e071c102a9877224f93658e370e54c
Gaurav <gauravsubhash9999@gmail.com>
2026-09-13 15:48:54 +0530

    done

---
+++ basic/Operators/01-arithimatic.js
@@ -0,0 +1,13 @@
+// Create a JavaScript program that takes two numbers and performs all basic arithmetic operations.
+
+// Take two numbers
+let num1 = Number(prompt("Enter the first number:"));
+let num2 = Number(prompt("Enter the second number:"));
+
+// Perform arithmetic operations
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Remainder:", num1 % num2);
+console.log("Exponentiation:", num1 ** num2);
