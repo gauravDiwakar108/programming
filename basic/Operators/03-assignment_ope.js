b739ee8616c88f8b0781219d09eb0ab44828e012
Gaurav <gauravsubhash9999@gmail.com>
2026-09-15 00:51:28 +0530

    done

---
+++ basic/Operators/03-assignment_ope.js
@@ -0,0 +1,23 @@
+// JavaScript program using assignment operators
+
+let num = Number(prompt("Enter a number:"));
+
+console.log("Initial value:", num);
+
+num = num + 5;
+console.log("After = :", num);
+
+num += 10;
+console.log("After += 10:", num);
+
+num -= 3;
+console.log("After -= 3:", num);
+
+num *= 2;
+console.log("After *= 2:", num);
+
+num /= 4;
+console.log("After /= 4:", num);
+
+num %= 3;
+console.log("After %= 3:", num);
