2d92d2a9dc42c30f178595420ad70de2435729cc
Gaurav <gauravsubhash9999@gmail.com>
2026-09-15 01:38:31 +0530

    done

---
+++ basic/Control Flow/03-weekday.js
@@ -0,0 +1,32 @@
+// program that takes a number from the user and uses a switch statement to print the corresponding day of the week
+
+let prompt = require('prompt-sync')();
+
+let day = Number(prompt("Enter Day -> "));
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Please Enter a valid day number");
+        break;
+}
