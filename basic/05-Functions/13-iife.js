a1e10c6bf3e19265d664b114549da59e9d997f51
Gaurav <gauravsubhash9999@gmail.com>
2026-09-24 19:56:00 +0530

    done

---
+++ basic/05-Functions/13-iife.js
@@ -0,0 +1,6 @@
+// Create an IIFE that creates a private variable message with the value "Hello JavaScript" and immediately prints it to the console.
+
+(function () {
+    let message = "Hello JavaScript"
+    console.log(message);
+})();
